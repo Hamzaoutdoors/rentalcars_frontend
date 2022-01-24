4fa88f33d79c7ca71f52cb14d0e481eb9b7077ae
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-24 19:16:21 -0300

    feature: create carSlice and data helper to populate initial state

---
+++ src/redux/cars/helpers/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    name: 'renegade',
+    brand: 'jeep',
+    image_url: 'https://i.ibb.co/M6DqDcJ/2.png',
+  },
+  {
+    name: 'escort',
+    brand: 'ford',
+    image_url: 'https://i.ibb.co/Hpq2DCn/4.png',
+  },
+  {
+    name: 'palio',
+    brand: 'fiat',
+    image_url: 'https://i.ibb.co/bvyvCC0/1.png',
+  },
+  {
+    name: 'golf',
+    brand: 'vw',
+    image_url: 'https://i.ibb.co/bvyvCC0/1.png',
+  },
+  {
+    name: 'clio',
+    brand: 'renault',
+    image_url: 'https://i.ibb.co/bvyvCC0/4.png',
+  },
+  {
+    name: 'civic',
+    brand: 'honda',
+    image_url: 'https://i.ibb.co/bvyvCC0/2.png',
+  },
+];
+
+export default data;
