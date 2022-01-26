05e78d7cc36f90f09fcf4a8bdc15eb45c969a47f
Hamza <hamzaoutdoors@gmail.com>
2022-01-26 01:37:29 +0100

    feat: Add propTypes and logo to carCard

--- src/components/cars/CarCard.js
+++ src/components/cars/CarCard.js
@@ -1,7 +1,10 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/forbid-prop-types */
 import styled from 'styled-components';
-// import { mobile } from '../../responsive';
 // import { NavLink } from 'react-router-dom';
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+// import { mobile } from '../../responsive';
 
 const Info = styled.div`
    opacity: 0;
@@ -17,17 +20,20 @@ const Info = styled.div`
    align-items: center;
    transition: all 0.5s ease;
    cursor: pointer;
+   border-radius: 25px;
 `;
 
 const Container = styled.div`
     flex: 1;
-    margin: 5px;
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
     min-width: 300px;
     height: 350px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #f5fbfd;
+    background-color: white;
     position: relative;
     &:hover ${Info}{
         opacity: 1;
@@ -35,18 +41,19 @@ const Container = styled.div`
 `;
 
 const Image = styled.img`
-   height: 75%;
+   width: 100%;
+   height: 30%;
+   flex: 1;
    z-index: 2;
-    
 `;
 
 const Circle = styled.div`
-    width: 200px;
-    height: 200px;
+    width: 250px;
+    height: 250px;
     border-radius: 50%;
-    background-color: white;
+    background-color: ${(props) => props.bgColor};
     position: absolute;
-    
+    margin: -50px -30px 0 0;
 `;
 
 const Icon = styled.div`
@@ -66,13 +73,40 @@ const Icon = styled.div`
     }
 `;
 
-const Product = ({ item }) => {
-  const { img } = item;
+const Detail = styled.div`
+    width: 100%;
+    z-index: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 
+    & h3 {
+        font-size: 15px;
+        font-weight: bold;
+        color: black;
+        text-transform: uppercase;
+        text-align: center;
+        border-bottom: 2px dotted #D3D3D3;
+    }
+
+    & h4 {
+        font-size: 11px;
+        font-weight: bold;
+        color: #D3D3D3;
+        text-align: center;
+        margin-top: 5px;
+    }
+`;
+
+const CarCard = ({ item }) => {
+  const {
+    image_url, name, brand, color,
+  } = item;
   return (
     <Container>
-      <Circle />
-      <Image src={img} />
+      <Circle bgColor={color} />
+      <Image src={image_url} />
       <Info>
         <Icon>
           <ShoppingCartOutlined />
@@ -84,8 +118,22 @@ const Product = ({ item }) => {
           <FavoriteBorderOutlined />
         </Icon>
       </Info>
+      <Detail>
+        <h3>{name}</h3>
+        <h4>
+          {brand}
+        </h4>
+      </Detail>
     </Container>
   );
 };
 
-export default Product;
+export default CarCard;
+
+CarCard.propTypes = {
+  image_url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired,
+};
