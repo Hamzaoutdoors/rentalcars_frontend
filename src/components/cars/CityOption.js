63240f99a57dacec2bf000dcdf17db9e92fe592b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-28 17:17:55 -0300

    feature: add CityOption component

---
+++ src/components/cars/CityOption.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { uuid as uuidv4 } from 'uuidv4';
+
+const Select = styled.select`
+  background-color: white;
+  padding: 0.5rem;
+  align-self: flex-start;
+  border-radius: 5px;
+  color: grey;
+  border: 2px solid grey;
+  margin-bottom: 0.5rem;
+`;
+
+const CityOption = () => {
+  const { cities } = useSelector((state) => state.reservations);
+
+  return (
+    <Select>
+      { cities.map((city) => (
+        <option key={() => uuidv4()} value={city.id}>
+          { city.name }
+        </option>
+      ))}
+    </Select>
+  );
+};
+
+export default CityOption;
