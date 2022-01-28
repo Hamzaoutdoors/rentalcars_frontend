75c0ac5320f6e9d960cf0fc8ba496a8e4d2accf8
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-28 19:33:27 -0300

    feature: add styles to titles and text in AddCar component

--- src/pages/AddCar.js
+++ src/pages/AddCar.js
@@ -8,17 +8,27 @@ const Container = styled.div`
   align-items: center;
   padding: 1rem;
   min-heigth: 100vh;
-  width: 100vw;
+  width: 100%;
 `;
 
 const Title = styled.div`
   display: flex;
   justify-content: center;
-  border-bottom: 1px solid grey;
-  font-size: 24px;
+  border-bottom: 2px solid black;
   margin-bottom: 1rem;
+  font-size: 36px;
+  font-weight: 700;
+  font-family: 'Bebas Neue', 'Arial', cursive !important;
+  color: black;
 `;
 
+const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  padding: 0 2.5rem;
+  font-family: 'Bebas Neue', 'Arial', cursive !important;
+  color: black;
+`;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -30,14 +40,14 @@ const AddCar = () => (
     <Container>
       <Wrapper>
         <Title>
-          <p style={{ padding: '0 2.5rem' }}>
+          <p style={{ padding: '0 2.5rem', margin: 0 }}>
             Add a Car to rent
           </p>
         </Title>
-        <p style={{ fontSize: '16px', padding: '0 2.5rem' }}>
+        <SubTitle>
           Let&apos;s be partners, with a 2% fee you can annouce your car on our website!
           We are available in 10 cities!
-        </p>
+        </SubTitle>
       </Wrapper>
       <CarForm />
     </Container>
