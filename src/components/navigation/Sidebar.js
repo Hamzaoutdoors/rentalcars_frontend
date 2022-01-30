329fa592a5f44b453010a33bad3d976c95ac560e
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-30 12:15:04 -0300

    feature: add new SideBar component

---
+++ src/components/navigation/Sidebar.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import styled from 'styled-components';
+import DesktopBar from './DesktopBar';
+
+const Sidebar = () => (
+  <>
+    <DesktopBar />
+  </>
+);
+
+export default Sidebar;
