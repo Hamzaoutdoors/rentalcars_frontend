8eacd11458e5320094586b02c8e037f8a9d5c39f
Hamza <hamzaoutdoors@gmail.com>
2022-01-31 00:32:58 +0100

    add more features

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import AddCar from './pages/AddCar';
 import LifeStyle from './pages/LifeStyle';
 import CarsHome from './components/cars/CarsHome';
 import MyReservations from './pages/MyReservations';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import './App.css';
 
 const Container = styled.div`
@@ -34,11 +36,13 @@ const App = () =>
       <Container>
         <Sidebar />
         <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign_up" element={<Register />} />
           <Route path="/cars" element={<CarsHome />} />
           <Route path="/myreservations" element={<MyReservations />} />
           <Route path="/:car_id/details" element={<DetailsPage />} />
           <Route path="/lifestyle" element={<LifeStyle />} />
-          <Route path="/" exact element={<HomePage />} />
           <Route path="/cars/new" element={<AddCar />} />
         </Routes>
       </Container>
