4d39f7a3937b3ea8bc7384ee7939661f15c8bc0a
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-30 11:52:51 -0300

    feature: add linkActive component that display an active link based on with route the user is.

---
+++ src/components/navigation/links/LinkActive.js
@@ -0,0 +1,58 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const LinkElem = styled(NavLink)`
+  font-size: 16px;
+  font-weight: 400;
+  font-weight: bold;
+  padding: 0.5rem 0 0.5rem 1rem;
+  text-decoration: none;
+  color: inherit;
+  &:hover {
+    color: black;
+  }
+`;
+
+const ActiveWrapper = styled.div`
+  background-color: #ED7730;
+  padding: 0.5rem 0rem 0.5rem 0rem;
+  width: 100%;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  color: white;
+`;
+
+const LinkActive = (props) => {
+  const {
+    path, text, active, setActive,
+  } = props;
+  const location = useLocation();
+
+  if (!active && path === location.pathname) setActive(path);
+
+  if (active === path) {
+    return (
+      <ActiveWrapper>
+        <LinkElem to={path} onClick={() => setActive(path)}>
+          Reserve a Car
+        </LinkElem>
+      </ActiveWrapper>
+    );
+  }
+
+  return (
+    <LinkElem to={path} onClick={() => setActive(path)}>
+      {text}
+    </LinkElem>
+  );
+};
+
+LinkActive.propTypes = {
+  path: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  active: PropTypes.string.isRequired,
+  setActive: PropTypes.func.isRequired,
+};
+
+export default LinkActive;
