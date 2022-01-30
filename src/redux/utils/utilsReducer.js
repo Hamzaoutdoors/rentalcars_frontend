b40164a635e76d77731e474141c8fbf6b110b128
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-30 19:54:25 -0300

    feature: add cases for set_size and item_index in utils reducer

--- src/redux/utils/utilsReducer.js
+++ src/redux/utils/utilsReducer.js
@@ -1,3 +1,5 @@
+import { SET_ITEM_SIZE, SET_SLIDER_INDEX } from './actions/sliderActions';
+
 const initialState = {
   navBar: {
     expandMobile: false,
@@ -12,6 +14,16 @@ const initialState = {
 
 const utilReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_ITEM_SIZE: {
+      const newState = { ...state };
+      newState.slider.slideIndex = action.payload;
+      return { ...newState };
+    }
+    case SET_SLIDER_INDEX: {
+      const newState = { ...state };
+      newState.slider.itemsSize = action.payload;
+      return { ...newState };
+    }
     default:
       return state;
   }
