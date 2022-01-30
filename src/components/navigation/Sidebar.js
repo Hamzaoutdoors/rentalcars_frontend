792a933e6448878a38e551040b9209431b11fd58
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-30 18:15:47 -0300

    feature: add breakdown to render mobile version of navbar

--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -1,13 +1,42 @@
 /* eslint-disable max-len */
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+
 // import { NavLink } from 'react-router-dom';
 // import styled from 'styled-components';
-import DesktopBar from './DesktopBar';
+import DesktopBar from './desktop/DesktopBar';
+import MobileBar from './mobile/MobileBar';
 
-const Sidebar = () => (
-  <>
-    <DesktopBar />
-  </>
-);
+const handleResize = () => {
+  if (window.innerWidth > 1150) {
+    return false;
+  }
+
+  return true;
+};
+
+const Sidebar = () => {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    setMobile(handleResize());
+    window.addEventListener('resize', () => {
+      setMobile(handleResize());
+    });
+  }, []);
+
+  if (mobile) {
+    return (
+      <>
+        <MobileBar />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <DesktopBar />
+    </>
+  );
+};
 
 export default Sidebar;
