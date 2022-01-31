8f16699557ff21f91ea947a836ea71b3443a0e07
Hamza <hamzaoutdoors@gmail.com>
2022-01-31 01:26:06 +0100

    feat : Seperate navigation routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import {
   Route,
 } from 'react-router-dom';
 import styled from 'styled-components';
-import DetailsPage from './pages/DetailsPage';
 // import { getCars } from './redux/cars/carsSlice';
 // import { getCities, getReservations } from './redux/reservations/reservationsSlice';
 import Announcement from './components/Announcement';
 import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
-import AddCar from './pages/AddCar';
-import LifeStyle from './pages/LifeStyle';
-import CarsHome from './components/cars/CarsHome';
-import MyReservations from './pages/MyReservations';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import links from './navigation';
+
 import './App.css';
 
 const Container = styled.div`
@@ -36,14 +31,10 @@ const App = () =>
       <Container>
         <Sidebar />
         <Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign_up" element={<Register />} />
-          <Route path="/cars" element={<CarsHome />} />
-          <Route path="/myreservations" element={<MyReservations />} />
-          <Route path="/:car_id/details" element={<DetailsPage />} />
-          <Route path="/lifestyle" element={<LifeStyle />} />
-          <Route path="/cars/new" element={<AddCar />} />
+          <Route exact path="/" element={<HomePage />} />
+          {links.map((link) => (
+            <Route key={link.id} path={link.path} element={link.element} />
+          ))}
         </Routes>
       </Container>
     </Router>
