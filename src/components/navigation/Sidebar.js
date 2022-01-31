2b4bffbf760cad8b1c0ecb63b09aa1ce1dd9c8d6
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-31 08:07:45 -0300

    feature: change! import actions and dispatch them to the store

--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -2,32 +2,36 @@ import { React, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import DesktopBar from './desktop/DesktopBar';
 import MobileBar from './mobile/MobileBar';
+import store from '../../redux/configureStore';
+import { setActiveDashLink, toggleMobile, toggleMobileModal } from '../../redux/utils/actions/navActions';
+
+const setDefaultWhenUnmount = () => {
+  store.dispatch(toggleMobile(false));
+  store.dispatch(toggleMobileModal(false));
+  store.dispatch(setActiveDashLink('cars'));
+};
 
 const handleResize = () => {
   if (window.innerWidth > 1150) {
-    return false;
+    store.dispatch(toggleMobile(false));
   }
 
-  return true;
+  store.dispatch(toggleMobile(true));
 };
 
 const Sidebar = () => {
   const { isMobile } = useSelector((state) => state.utils.navbar);
 
   useEffect(() => {
-    setMobile(handleResize());
+    handleResize();
     window.addEventListener('resize', () => {
-      setMobile(handleResize());
+      handleResize();
     });
+
+    return () => {
+      setDefaultWhenUnmount();
+    };
   }, []);
-
-  // useEffect(() => {
-  //   first;
-
-  //   return () => {
-  //     second;
-  //   };
-  // }, [third]);
 
   if (isMobile) {
     return (
