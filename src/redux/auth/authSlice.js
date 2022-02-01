e2d5dd4b7a3b647523621524ac0c65e7af6fe614
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-01 19:17:10 -0300

    feature: remove duplicated actions and change authenticate to receive in the payload both formElem and api_endpoint

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,20 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const LOGIN_ENDPOINT = 'http://localhost:4000/login';
-const SIGNUP_ENDPOINT = 'http://localhost:4000/signup';
+export const LOGIN_ENDPOINT = 'http://localhost:4000/login';
+export const SIGNUP_ENDPOINT = 'http://localhost:4000/signup';
 
 const initialState = {
   isFetching: false,
   isAuthenticated: false,
-  token: null,
   user: null,
   error: {},
 };
 
-const authenticateBodyOptions = (formData) => {
-  const keys = [...formData.keys()];
-  const values = [...formData.values()];
+const authenticateBodyOptions = (formElem) => {
+  const data = new FormData(formElem);
+  const keys = [...data.keys()];
+  const values = [...data.values()];
 
   const body = {
     user: {
@@ -34,50 +34,50 @@ const authenticateBodyConfig = () => ({
   },
 });
 
-const addAuthenticateBodyOptions = (formData) => {
-  const keys = [...formData.keys()];
-  const values = [...formData.values()];
-
-  const body = {
-    user: {
-    },
-  };
-
-  keys.forEach((key, index) => {
-    body.user[key] = values[index];
-  });
-
-  return body;
-};
-
-const addAuthenticateBodyConfig = () => ({
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export const authenticateUser = createAsyncThunk(
   'redux/cars/login',
   async (payload) => {
-    const data = authenticateBodyOptions(payload);
+    const data = authenticateBodyOptions(payload.form);
     const config = authenticateBodyConfig();
 
     const response = await axios
-      .post(LOGIN_ENDPOINT, data, config)
+      .post(payload.url, data, config)
       .catch((error) => error);
+
+    localStorage.setItem('rcars_jwt', JSON.stringify(response.data.token));
     return response.data;
   },
 );
 
-export const addAuthenticateUser = createAsyncThunk(
-  'redux/auth/signup',
-  async (payload) => {
-    const data = addAuthenticateBodyOptions(payload);
-    const config = addAuthenticateBodyConfig();
-
-    const response = await axios
-      .post(SIGNUP_ENDPOINT, data, config)
-      .catch((error) => error);
-    return response.data;
+const authSLice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logOutUser: () => ({
+      isFetching: false,
+      isAuthenticated: false,
+      user: null,
+      error: {},
+    }),
   },
-);
+  extraReducers: {
+    [authenticateUser.pending.type]: (state) => ({
+      ...state, isFetching: true, isAuthenticated: false, user: null, error: {},
+    }),
+    [authenticateUser.fulfilled.type]: (state, action) => (
+      {
+        ...state,
+        isFetching: false,
+        isAuthenticated: true,
+        user: { ...action.payload.user },
+        error: {},
+      }),
+    [authenticateUser.rejected.type]: (state, action) => (
+      {
+        ...state, isFetching: false, isAuthenticated: false, error: { ...action.payload },
+      }),
+  },
+});
+
+export const { logOutUser } = authSLice.actions;
+export default authSLice.reducer;
