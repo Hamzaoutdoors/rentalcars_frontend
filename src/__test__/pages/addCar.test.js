d781367d815f9c8180ba04a6c29d0a10cb86c0a4
Hamza <hamzaoutdoors@gmail.com>
2022-02-04 00:58:29 +0100

    feat : Add unit tests for all pages views

---
+++ src/__test__/pages/addCar.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import AddCar from '../../pages/AddCar';
+import store from '../../redux/ConfigureStore';
+
+it('render the Add Car page correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Router>
+          <AddCar />
+        </Router>
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/__test__/pages/myCars.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import MyCars from '../../pages/MyCars';
+import store from '../../redux/ConfigureStore';
+
+it('render the cars list page correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Router>
+          <MyCars />
+        </Router>
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
