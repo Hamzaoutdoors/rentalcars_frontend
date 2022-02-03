cf690686d0bbc800fba815fa221f71eb04d62266
Hamza <hamzaoutdoors@gmail.com>
2022-02-03 04:54:48 +0100

    feat : Add all actions creator for reservations

--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -3,58 +3,88 @@ import axios from 'axios';
 
 import data from './helpers/data';
 
-const RESERVATIONS_API_ENDPOINT = 'https://something/api/v1/:user_id/reservations';
-const CITIES_API_ENDPOINT = 'https://something/api/v1/cities';
+const RESERVATIONS_API_ENDPOINT = 'http://127.0.0.1:4000/api/v1/reservations';
+const CITIES_API_ENDPOINT = 'http://127.0.0.1:4000/cities';
 
 const initialState = {
   isFetching: false,
-  data: [...data.reservations],
+  data: [],
   cities: [...data.cities],
   error: {},
-  utils: {
-    openModal: false,
-  },
+};
+
+// eslint-disable-next-line no-unused-vars
+const jsonTypeConfig = (token) => {
+  if (token) {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    };
+  }
+
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 };
 
 export const getReservations = createAsyncThunk(
   'redux/cars/getReservations.js',
-  async () => {
-    const response = await axios.get(RESERVATIONS_API_ENDPOINT).catch((error) => error);
-    return response.data;
+  async (payload, { rejectWithValue }) => {
+    try {
+      const res = await axios.get(RESERVATIONS_API_ENDPOINT);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue({ ...error.response.data });
+    }
   },
 );
 
 export const getCities = createAsyncThunk(
   'redux/cars/getCities.js',
-  async () => {
-    const response = await axios.get(CITIES_API_ENDPOINT).catch((error) => error);
-    return response.data;
+  async (payload, { rejectWithValue }) => {
+    try {
+      const res = await axios.get(CITIES_API_ENDPOINT);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue({ ...error.response.data });
+    }
   },
 );
 
 export const addReservation = createAsyncThunk(
   'redux/cars/addReservation.js',
-  async () => {
-    // should be like this axios.post(url, addReservationParams, addReservationHeaders)
-    const response = await axios.post(RESERVATIONS_API_ENDPOINT)
-      .catch((error) => error);
-    return response.data;
+  async (payload, { rejectWithValue }) => {
+    // payload: { formElems } === {start_date, end_date, city_id, car_id}
+    const token = localStorage.getItem('rcars_jwt');
+    const config = jsonTypeConfig(token);
+    try {
+      const res = await axios.post(RESERVATIONS_API_ENDPOINT, payload, config);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue({ ...error.response.data });
+    }
   },
 );
 
-const deleteOptions = {
-  headers: {
-    something: '',
-  },
+const filterDeleted = (data, id) => {
+  data.filter((reservationObj) => reservationObj.id !== parseInt(id, 10));
 };
 
 export const cancelReservation = createAsyncThunk(
   'redux/cars/removeReservation.js',
-  async (id) => {
-    const response = await axios
-      .delete(`${RESERVATIONS_API_ENDPOINT}/${id}`, deleteOptions)
-      .catch((error) => error);
-    return response.data;
+  async (payload, { rejectWithValue }) => {
+    const token = localStorage.getItem('rcars_jwt');
+    const config = jsonTypeConfig(token);
+    try {
+      const res = await axios.delete(`${RESERVATIONS_API_ENDPOINT}/${payload}`, config);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue({ ...error.response.data });
+    }
   },
 );
 
@@ -80,6 +110,36 @@ const reservationsSlicer = createSlice({
     },
   },
   extraReducers: {
+    [getReservations.pending.type]: (state) => ({ ...state, isFetching: true, error: {} }),
+    [getReservations.fulfilled.type]: (state, action) => (
+      {
+        ...state, isFetching: false, data: action.payload, error: {},
+      }),
+    [getReservations.rejected.type]: (state, action) => (
+      { ...state, isFetching: false, error: action.payload }
+    ),
+    [getReservations.pending.type]: (state) => (
+      { ...state, isFetching: true, error: {} }
+    ),
+    [addReservation.fulfilled.type]: (state, action) => (
+      { ...state, isFetching: false, data: [...state.data, action.payload] }
+    ),
+    [addReservation.rejected.type]: (state, action) => (
+      { ...state, isFetching: false, error: { ...action.payload } }
+    ),
+    [cancelReservation.pending.type]: (state) => (
+      { ...state, isFetching: true, error: {} }
+    ),
+    [cancelReservation.fulfilled.type]: (state, action) => (
+      {
+        ...state,
+        isFetching: false,
+        data: filterDeleted(state.data, action.payload.id),
+      }
+    ),
+    [cancelReservation.rejected.type]: (state, action) => (
+      { ...state, isFetching: false, error: { ...action.payload } }
+    ),
   },
 });
 
