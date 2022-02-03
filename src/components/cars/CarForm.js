5c98c4e5292a2fb7fccc685e035f68fb1db2c6c8
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-02 21:30:26 -0300

    feat: dispatch form data to the API when submitting data

--- src/components/cars/CarForm.js
+++ src/components/cars/CarForm.js
@@ -1,5 +1,7 @@
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { mobile } from '../../responsive';
+import { addCar } from '../../redux/cars/carsSlice';
 
 const Container = styled.form`
   display: flex;
@@ -32,6 +34,14 @@ const InputLabel = styled.div`
   margin-bottom: 1rem;
 `;
 
+const Label = styled.div`
+  font-family: 'Urbanist', 'Arial', cursive;
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  margin: 1rem;
+`;
+
 const Input = styled.input.attrs((props) => ({
   placeholder: props.placeholder,
   defaultValue: props.value || '',
@@ -49,56 +59,76 @@ const Input = styled.input.attrs((props) => ({
   box-shadow: 0px 3px 7px -2px rgba(79,79,79,0.64);
 `;
 
-const AnnounceButton = styled.a`
-  padding: 0.6rem 1.5rem;
-  border-radius: 20px;
-  width: 150px;
-  background-color: #97BF11;
-  text-decoration: none;
-  text-align: center !important;
+const AnnounceButton = styled.button`
   justify-self: end;
   align-self: center;
+  width: 150px;
+  background-color: #97BF11;
+  padding: 0.6rem 1.5rem;
+  border-radius: 20px;
+  border: none;
+  margin-bottom: 1rem;
+  margin-top: auto;
   font-family: 'Roboto', 'Arial', sans-serif;
   font-size: 16px;
   font-weight: 500;
+  text-align: center !important;
   color: grey;
   color: white !important;
-  margin-bottom: 1rem;
-  margin-top: auto;
   &:hover {
     color: white !important;
+    background-color: #97BF11;
     cursor: pointer;
   } 
 `;
 
-const CarForm = () => (
-  <Container>
-    <FormRow>
-      <InputWrapper>
-        <InputLabel>
-          Car Info
-        </InputLabel>
-        <Input placeholder="Model" id="car-model" />
-        <Input placeholder="Brand" id="car-brand" />
-        <Input placeholder="Image Url" id="car-image" />
-      </InputWrapper>
-    </FormRow>
-    <FormRow>
-      <InputWrapper>
-        <InputLabel>
-          Car Details
-        </InputLabel>
-        <Input placeholder="Price Daily" id="price-daily" />
-        <Input placeholder="Price Monthly" id="price-monthly" />
-        <Input placeholder="Main color (hex value)" id="car-color" />
-      </InputWrapper>
-      <InputWrapper>
-        <AnnounceButton onClick={() => console.log('I was clicked')}>
-          Announce
-        </AnnounceButton>
-      </InputWrapper>
-    </FormRow>
-  </Container>
-);
+const CarForm = () => {
+  const dispatch = useDispatch();
+  const submitCar = (event) => {
+    event.preventDefault();
+    dispatch(addCar(event.target));
+  };
+
+  return (
+    <Container onSubmit={(e) => submitCar(e)}>
+      <FormRow>
+        <InputWrapper>
+          <InputLabel>
+            Car Info
+          </InputLabel>
+          <Input type="text" name="name" placeholder="Model" id="car-model" />
+          <Input type="text" name="brand" placeholder="Brand" id="car-brand" />
+          <Input type="text" name="imgUrl" placeholder="Image Url" id="car-image" />
+        </InputWrapper>
+      </FormRow>
+      <FormRow>
+        <InputWrapper>
+          <InputLabel>
+            Car Details
+          </InputLabel>
+          <Input type="number" step="0.01" name="price_daily" placeholder="Price Daily" id="price-daily" />
+          <Input type="number" step="0.01" name="price_monthly" placeholder="Price Monthly" id="price-monthly" />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Label>
+              Color:
+            </Label>
+            <Input
+              type="color"
+              name="color"
+              placeholder="Main color (hex value)"
+              id="car-color"
+              style={{ width: '100px', height: '35px' }}
+            />
+          </div>
+        </InputWrapper>
+        <InputWrapper>
+          <AnnounceButton type="submit">
+            Announce
+          </AnnounceButton>
+        </InputWrapper>
+      </FormRow>
+    </Container>
+  );
+};
 
 export default CarForm;
