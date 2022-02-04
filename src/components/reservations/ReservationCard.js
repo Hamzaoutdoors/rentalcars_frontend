09b14f1c0d99e0540a82ded1a866816e0574c29d
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-04 12:38:55 -0300

    feat: fix jwt localstorage token and select option

--- src/components/reservations/ReservationCard.js
+++ src/components/reservations/ReservationCard.js
@@ -118,7 +118,7 @@ const ReservationCard = ({ reservationDetail }) => {
   const carName = car.name;
   const image = car.imgUrl;
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth.user.payload);
 
   return (
     <CardContainer
